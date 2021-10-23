25ad529ecbe7cb92ff9861c27df1bb2401068b4a
Chris Wong <wh18710455626@126.com>
2021-10-23 22:30:59 +0800

    cross-domain

---
+++ mall/vue.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    // node.js knowledge(about config)
+    devServer={
+        host: 'localhost',
+        port: 8080,
+        proxy: { // intercept and tweet/redirect
+            '/activity': {
+                target:'https://www.imooc.com',
+                changeOrigin: true, //是否将主机头改为目标的url地址
+                pathRewrite: {
+                    '/activity': '/activity'
+                }
+
+
+            }
+        }
+    }
+}
