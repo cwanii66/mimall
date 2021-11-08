d6a5305d6d87726bd0750caf439f390c31dd50d7
Chris Wong <wh18710455626@126.com>
2021-11-08 11:16:45 +0800

    swiper

--- mall/src/pages/index.vue
+++ mall/src/pages/index.vue
@@ -1,5 +1,31 @@
 <template>
-    <div>
+    <div class="main-content">
+        <div class="container">
+            <div class="swiper-box">
+                <swiper :options="swiperOption" ref="mySwiper">
+                    <swiper-slide
+                        v-for="(item, index) in slideList"
+                        :key="index"
+                    >
+                        <a :href="'/#/product' + item.id"><img :src="item.img" alt="#"></a>
+                    </swiper-slide>
+
+                    <!-- Optional controls -->
+                    <div class="swiper-pagination" slot="pagination"></div>
+                    <div class="swiper-button-prev" slot="button-prev"></div>
+                    <div class="swiper-button-next" slot="button-next"></div>
+                </swiper>
+            </div>
+            <div class="ads-box">
+
+            </div>
+            <div class="banner">
+
+            </div>
+            <div class="product-box">
+
+            </div>
+        </div>
         <service-bar></service-bar>
     </div>
 </template>
@@ -7,16 +33,80 @@
 <script>
 // this page embed in router-view
 import ServiceBar from './../components/ServiceBar.vue'
+import {Swiper, SwiperSlide, directive} from 'vue-awesome-swiper'
+import 'swiper/css/swiper.css'
 
 export default {
     name: 'index',
 
+    data() {
+        return {
+            swiperOption: {
+                autoplay: true,
+                loop: true,
+                effect: 'cube',
+                cubeEffect: {
+                    slideShadows: true,
+                    shadow: true,
+                    shadowOffset: 20,
+                    shadowScale: 0.6
+                },
+                pagination: {
+                    el: '.swiper-pagination',
+                    clickable: true
+                },
+                navigation: {
+                    nextEl: '.swiper-button-next',
+                    prevEl: '.swiper-button-prev',
+                }
+            },
+            slideList: [
+                {
+                    id: '42',
+                    img: '/imgs/slider/slide-1.jpg'
+                },
+                {
+                    id: '45',
+                    img: '/imgs/slider/slide-2.jpg'
+                },
+                {
+                    id: '46',
+                    img: '/imgs/slider/slide-3.jpg'
+                },
+                {
+                    id: '',
+                    img: '/imgs/slider/slide-4.jpg'
+                },
+                {
+                    id: '',
+                    img: '/imgs/slider/slide-5.jpg'
+                }
+            ]
+        }
+    },
+
     components: {
-        ServiceBar
+        ServiceBar,
+        Swiper,
+        SwiperSlide
+    },
+    directives: {
+        swiper: directive
     }
 }
 </script>
 
 <style lang="scss">
+
+    .main-content {
+        .swiper-box {
+            .swiper-container {
+                height: 451px;
+                img {
+                    width: 100%;
+                }
+            }
+        }
+    }
     
 </style>
