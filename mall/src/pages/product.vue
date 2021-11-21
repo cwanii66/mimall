ae3ec0cb47700c9a144d54d276e6279c328e5c44
Chris Wong <wh18710455626@126.com>
2021-11-21 15:02:28 +0800

    product-param-component-sty

--- mall/src/pages/product.vue
+++ mall/src/pages/product.vue
@@ -1,11 +1,32 @@
 <template>
-    <div>
-        product
+    <div class="product">
+        <product-params>
+            <template #buy>
+                <button class="btn">立即购买</button>
+            </template>
+        </product-params>
+        
     </div>
 </template>
 
 <script>
+import ProductParams from './../components/ProductParams.vue'
+
 export default {
-    name: 'product'
+    name: 'product',
+
+    components: {
+        ProductParams
+    }
 }
 </script>
+
+<style lang="scss">
+
+.product {
+    button {
+        margin-left: 10px;
+    }
+}
+
+</style>
