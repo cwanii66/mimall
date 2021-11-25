3c0a56a24d4215316dd8072e172c94639dd7860c
Chris Wong <wh18710455626@126.com>
2021-11-25 12:30:11 +0800

    orderheader 组件复用

--- mall/src/pages/order.vue
+++ mall/src/pages/order.vue
@@ -1,21 +1,49 @@
 <template>
     <div>
-        <order-header></order-header>
+        <order-header :title="title">
+            <template #tips>
+                <span>{{tip}}</span>
+            </template>
+        </order-header>
         <router-view></router-view>
+        <service-bar></service-bar>
         <nav-footer></nav-footer>
     </div>
 </template>
 
 <script>
 import OrderHeader from '../components/OrderHeader.vue'
+import ServiceBar from '../components/ServiceBar.vue'
 import NavFooter from '../components/NavFooter.vue'
 
 export default {
     name: 'order',
     components: {
         OrderHeader,
+        ServiceBar,
         NavFooter
-    }
+    },
+
+    data() {
+        return {
+            title: '',
+            tip: ''
+        }
+    },
+
+    mounted() {
+        let path = this.$route.path;
+        if (path === '/order/confirm') {
+            this.title = '订单确认';
+            this.tip = '请认真填写收货地址'
+        } else if (path === '/order/list' ) {
+            this.title = '订单列表';
+            this.tip = '请谨防钓鱼链接或诈骗电话，了解更多 >'
+        } else if (path === '/order/pay') {
+            this.title = '订单支付';
+            this.tip = '请谨防钓鱼链接或诈骗电话，了解更多 >'
+        }
+    },
 }
 </script>
 
--- mall/src/router/index.js
+++ mall/src/router/index.js
@@ -66,7 +66,7 @@ const routes = [
         component: OrderList,
       },
       {
-        path: 'comfirm',
+        path: 'confirm',
         name: 'confirm',
         component: OrderComfirm,
       },
