c4184d6578868f4a75e24b5a6004974a69ae8877
Chris Wong <wh18710455626@126.com>
2021-11-30 09:57:41 +0800

    orderSubmit&AddressManagement

--- mall/src/pages/orderConfirm.vue
+++ mall/src/pages/orderConfirm.vue
@@ -25,9 +25,10 @@
           <div class="item-address">
             <h2 class="addr-title">收货地址</h2>
             <div class="addr-list clearfix">
-              <div class="addr-info"
-                v-for="item in list"
+              <div class="addr-info" :class="{'checked': index === checkedId}"
+                v-for="(item, index) in list"
                 :key="item.id"
+                @click="checkedId = index"
               >
                 <h2> {{ item.receiverName }} </h2>
                 <div class="phone"> {{ item.receiverMobile }} </div>
@@ -36,12 +37,12 @@
                   <a href="javascript:;" class="fl" @click="delAddress(item)">
                     <svg class="icon icon-del"><use xlink:href="#icon-del"></use></svg>
                   </a>
-                  <a href="javascript:;" class="fr">
+                  <a href="javascript:;" class="fr" @click="editAddressModal(item)">
                     <svg class="icon icon-edit"><use xlink:href="#icon-edit"></use></svg>
                   </a>
                 </div>
               </div>
-              <div class="addr-add">
+              <div class="addr-add" @click="openAddressModal">
                 <div class="icon-add"></div>
                 <div>添加新地址</div>
               </div>
@@ -96,7 +97,7 @@
           </div>
           <div class="btn-group">
             <a href="/#/cart" class="btn btn-default btn-large">返回购物车</a>
-            <a href="javascript:;" class="btn btn-large">去结算</a>
+            <a href="javascript:;" class="btn btn-large" @click="orderSubmit">去结算</a>
           </div>
         </div>
       </div>
@@ -112,21 +113,21 @@
       <template #body>
         <div class="edit-wrapper">
           <div class="item">
-            <input type="text" class="input" placeholder="姓名">
-            <input type="text" class="input" placeholder="手机号">
+            <input type="text" class="input" placeholder="姓名" v-model="checkedItem.receiverName">
+            <input type="text" class="input" placeholder="手机号" v-model="checkedItem.receiverMobile">
           </div>
           <div class="item">
-            <select name="province">
+            <select name="province" v-model="checkedItem.receiverProvince">
               <option value="北京">北京</option>
               <option value="天津">天津</option>
               <option value="河北">河北</option>
             </select>
-            <select name="city">
+            <select name="city" v-model="checkedItem.receiverCity">
               <option value="北京">北京</option>
               <option value="天津">天津</option>
               <option value="河北">石家庄</option>
             </select>
-            <select name="district">
+            <select name="district" v-model="checkedItem.receiverDistrict">
               <option value="北京">昌平区</option>
               <option value="天津">海淀区</option>
               <option value="河北">朝阳区</option>
@@ -136,10 +137,10 @@
             </select>
           </div>
           <div class="item">
-            <textarea name="street" placeholder="详细地址"></textarea>
+            <textarea name="street" placeholder="详细地址" v-model="checkedItem.receiverAddress"></textarea>
           </div>
-          <div class="item">
-            <input type="text" class="input" placeholder="邮编">
+          <div class="item" >
+            <input type="text" class="input" placeholder="邮编" v-model="checkedItem.receiverZip">
           </div>
         </div>
       </template>
@@ -175,10 +176,11 @@ export default {
             cartList: [], // 购物车中需要结算的商品列表
             cartTotalPrice: 0, // 商品总金额
             count: 0, // 商品结算数量
-            checkedItem: {}, // 选中的商品对象
+            checkedItem: {}, // 选中的对象
             userAction: '', // 用户的行为， 0：新增、1：编辑、2：删除
             showDelModal: false, // 是否显示删除弹框
-            showEditModal: true, // 是否显示新增活着编辑弹框
+            showEditModal: false, // 是否显示新增活着编辑弹框
+            checkedId: 0, // 当前选中收货地址索引
         }
     },
 
@@ -193,7 +195,19 @@ export default {
                 this.list = res.list;
             })
         },
-        
+        // 打开新增地址弹框
+        openAddressModal() {
+          this.userAction = 0;
+          this.checkedItem = {};
+          this.showEditModal = true;
+        },
+        // 编辑地址弹框
+        editAddressModal(item) {
+          this.userAction = 0;
+          this.checkedItem = item;  // checkedItem作为数据中间容器
+          this.showEditModal = true;
+        },
+        // 删除地址
         delAddress(item) {
           this.checkedItem = item;
           this.userAction = 2;
@@ -202,24 +216,64 @@ export default {
         // 地址删除、编辑、新增功能
         submitAddress() {
           let {checkedItem, userAction} = this;
-          let method, url;
-          switch (userAction) {
+          let method, url, params = {};
+          switch(userAction) {
             case 0:
               method = 'post', url = '/shippings';
               break;
-            case 1:
+            case 1: 
               method = 'put', url = `/shippings/${checkedItem.id}`;
               break;
             case 2:
               method = 'delete', url = `/shippings/${checkedItem.id}`;
               break;
+            default:
+              break;
           }
-
-          this.axios[method](url).then((res) => {
+          if (userAction == 0 || userAction == 1) {
+            let {
+                receiverName,
+                receiverMobile,
+                receiverProvince,
+                receiverCity,
+                receiverDistrict,
+                receiverAddress,
+                receiverZip,
+              } = checkedItem;
+              let errMsg = '';
+              if (!receiverName) {
+                errMsg = '请输入收货人名称';
+              } else if (!receiverMobile || !/(^1[3|5|8][0-9]{9}$)/.test(receiverMobile)) {
+                errMsg = '请输入正确的手机号';
+              } else if (!receiverProvince) {
+                errMsg = '请选择省份';
+              } else if (!receiverCity) {
+                errMsg = '请输入正确城市';
+              } else if (!receiverDistrict || !receiverAddress) {
+                errMsg = '请输入收货地址';
+              } else if (!receiverZip || !/\d{6}/.test(receiverZip)) {
+                errMsg = '请输入六位邮编';
+              }
+              if(errMsg) {
+                this.$message.error(errMsg);
+                return;
+              }
+              params = {
+                receiverName,
+                receiverMobile,
+                receiverProvince,
+                receiverCity,
+                receiverDistrict,
+                receiverAddress,
+                receiverZip,
+              };
+          }
+          // 地址栏操作
+          this.axios[method](url, params).then((res) => {
             this.closeModal();
             this.getAddressList();
-            // console.log(res);
-            this.$message.success(res);
+            console.log(res);
+            this.$message.success('操作成功');
           })
           
         },
@@ -228,6 +282,7 @@ export default {
           this.checkedItem = {};
           this.userAction = '';
           this.showDelModal = false;
+          this.showEditModal = false;
         },
 
         getCartList() {
@@ -239,9 +294,33 @@ export default {
                     this.count += item.quantity;
                 })
             })
-        }
-    }
+        },
+        // 订单提交
+        orderSubmit() {
+          let item = this.list[this.checkedId];
+          let cartCount = this.count;
+          if (!item) {
+            this.$message.error('请选择一个收货地址');
+            return;
+          }
+          if (!cartCount) {
+            this.$message.error('没有商品');
+            return;
+          }
 
+          this.axios.post('/orders', {   // 订单创建成功后商品数据就会被清空
+            shippingId: item.id
+          }).then((res) => {
+            this.$router.push({
+              path: '/order/pay',
+              query: {
+                orderNo: res.orderNo
+              }
+            })
+          })
+        }
+
+    }
 }
 </script>
 
